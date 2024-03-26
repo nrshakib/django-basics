bc2239648c72cb01b91dd3b2b787529012caa881
NRS\nrsha <nrshakib007@gmail.com>
2024-03-27 01:39:00 +0600

    Initialized Commit

---
+++ first_project/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for first_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+# Web Server Gateway Interface
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
+
+application = get_wsgi_application()
